dc16ef5bdba1620263b18ba5ea19cfbf073e3559
Christopher Cowan <chrisjoelcowan@gmail.com>
2025-12-24 21:00:37 -0500

    Responding to PING

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+use std::io::Write;
 use std::net;
 
 fn main() {
@@ -5,17 +7,25 @@ fn main() {
         Ok(listener) => {
             for stream in listener.incoming() {
                 match stream {
-                    Ok(_stream) => {
+                    Ok(mut stream) => {
                         println!("accepted new connection");
+                        match respond_to_connection(&mut stream) {
+                            Ok(_) => println!("closed connection"),
+                            Err(e) => eprintln!("error handling connection: {e}"),
+                        }
                     }
                     Err(e) => {
-                        println!("error accepting connection {}", e);
+                        eprintln!("error accepting connection {e}");
                     }
                 }
             }
         }
         Err(e) => {
-            eprintln!("error binding TCP listener: {}", e);
+            eprintln!("error binding TCP listener: {e}");
         }
     }
+}
+
+fn respond_to_connection(stream: &mut net::TcpStream) -> io::Result<()> {
+    write!(stream, "+PONG\r\n")
 }
